83a39f8e0b9ac62ca3f48528381ba24da89a5a6d
EfoJensen <Jensena231@gmail.com>
2025-11-08 14:06:14 +0000

    added join table for drugs and chemicals

--- src/migrations/20251104100708_create_drug_chemicals_table.ts
+++ src/migrations/20251104100708_create_drug_chemicals_table.ts
@@ -1,25 +1,36 @@
 import db from '../config/database/db'
 
 export async function up() {
+    const client = await db.connect()
     try {
-        await db.query(`
+        await client.query(`
             CREATE TABLE IF NOT EXISTS drug_chemicals(
                 drug_id INT REFERENCES drugs(id),
-                chemical_id INT REFERENCES chemicals(id),
-                quantity TEXT,
+                chemical_id INT REFERENCES chemicals(chemical_id),
+                quantity DECIMAL(10, 3) NOT NULL,
+                unit unit_enum NOT NULL,
                 PRIMARY KEY(drug_id, chemical_id)
-            )
+            );
         `)
     } catch (error) {
-        console.error(error)
+        await client.query('ROLLBACK');
+        console.error('Migration failed:', error);
+        throw error;
+    } finally {
+        client.release();
     }
 }
 
 export async function down() {
+    const client = await db.connect()
     try {
         await db.query('DROP TABLE IF NOT EXISTS drug_chemicals')
     } catch (error) {
-        console.error(error)
+        await client.query('ROLLBACK');
+        console.error('Migration failed:', error);
+        throw error;
+    } finally {
+        client.release();
     }
 }
 
