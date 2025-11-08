0ef10d078554596b17dab4e7565e5b33f212f4d8
EfoJensen <Jensena231@gmail.com>
2025-11-08 13:49:21 +0000

    successfully added chemicals table which will be used for chemical composition of drugs

--- src/migrations/20251104095912_create_chemicals_table.ts
+++ src/migrations/20251104095912_create_chemicals_table.ts
@@ -1,23 +1,45 @@
 import db from '../config/database/db'
 
 export async function up() {
+    const client = await db.connect()
     try {
-        await db.query(`
+        await client.query('BEGIN')
+
+        await client.query(`
             CREATE TABLE IF NOT EXISTS chemicals(
                 chemical_id SERIAL PRIMARY KEY,
-                name TEXT NOT NULL,
-            )
+                name TEXT NOT NULL
+            );
         `)
+
+        await client.query(`CREATE INDEX idx_chemicals_name ON chemicals(name)`)
+
+        await client.query('COMMIT');
     } catch (error) {
-        console.error(error)
+        await client.query('ROLLBACK');
+        console.error('Migration failed:', error);
+        throw error;
+    } finally {
+        client.release();
     }
 }
 
 export async function down() {
+    const client = await db.connect()
     try {
-        await db.query('DROP TABLE IF NOT EXISTS chemicals')
+        await client.query('BEGIN')
+
+        await client.query('DROP TABLE IF NOT EXISTS chemicals')
+
+        await client.query('DROP INDEX IF EXISTS idx_chemicals_name')
+
+        await client.query('COMMIT')
     } catch (error) {
-        console.error(error)
+        await client.query('ROLLBACK');
+        console.error('Migration failed:', error);
+        throw error;
+    } finally {
+        client.release();
     }
 }
 
